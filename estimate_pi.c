29c57185556b0a69afc460b81a3a3346951ea129
jmsegret <jms878@cornell.edu>
2017-09-29 14:25:30 -0400

    OpenMp works correctly

--- estimate_pi.c
+++ estimate_pi.c
@@ -11,10 +11,16 @@ int main(int argc, char **argv)
 {
 
    int niter = atoi(argv[1]);
-   
+  
+
    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
    /* ~~~~~~~~~~~~~~~~~PARALLELIZE ME~~~~~~~~~~~~~~~~~~ */
    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+   int count=0;
+
+   #pragma omp parallel shared(count)
+   {
+
 
    /* Random Number Generation via MKL Random Number Stream */        
    float * rand_buffer_x = (float*)malloc(batchsize*sizeof(float)); 
@@ -24,12 +30,15 @@ int main(int argc, char **argv)
    vslNewStream( &stream, VSL_BRNG_SFMT19937, SEED );
    
    /* Loop over Randomly Generated Numbers */
-   int count = 0;
+  
    int i,j;
+
+   #pragma omp for reduction(+:count)
    for(j = 0; j < niter; j+=batchsize)
    {
       vsRngUniform( VSL_RNG_METHOD_UNIFORM_STD, stream, batchsize, rand_buffer_x, 0.0, 1.0 );
       vsRngUniform( VSL_RNG_METHOD_UNIFORM_STD, stream, batchsize, rand_buffer_y, 0.0, 1.0 );
+  
       for(i = 0; i < batchsize; i++)
       {
          float z = rand_buffer_y[i]*rand_buffer_y[i] + rand_buffer_x[i]*rand_buffer_x[i];
@@ -38,18 +47,20 @@ int main(int argc, char **argv)
    }
 
 
+    
    /* Delete the stream and buffers*/        
    vslDeleteStream( &stream );
    free(rand_buffer_x);
    free(rand_buffer_y);
 
+   }
    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
    /* ~~~~~~~~~~~~~~~~~PARALLELIZE ME~~~~~~~~~~~~~~~~~~ */
    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 
    double pi = (double) count / niter*4;
-   printf("num trials=%d, estimate of pi=%f\n", niter, pi);
+   printf("num trials=%d, estimate of pi=%f\n" , niter, pi);
 
    return 0;
 }
